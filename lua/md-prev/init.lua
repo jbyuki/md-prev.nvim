f651d73308c0771a29cb1f67e215253aae4cef42
Julien Burkhard <jyburkhard@hotmail.com>
2022-09-26 00:22:16 +0200

    github css fix

--- lua/md-prev/init.lua
+++ lua/md-prev/init.lua
@@ -42,8 +42,28 @@ function M.start_server(port)
 
   			      local md = require("markdown")
   			      encoded = md(table.concat(lines, "\n"))
+  			      local before = [[<html>
+  			        <head>
+  			      <meta http-equiv="content-type" content="text/html; charset=windows-1252">
+  			      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css">
+  			      <style>
+  			      	.markdown-body {
+  			      		box-sizing: border-box;
+  			      		min-width: 200px;
+  			      		max-width: 980px;
+  			      		margin: 0 auto;
+  			      		padding: 45px;
+  			      	}
 
-  			      local before = [[<html> <head> <meta http-equiv="content-type" content="text/html; charset=windows-1252"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css"> </head> <body class="markdown-body">]]
+  			      	@media (max-width: 767px) {
+  			      		.markdown-body {
+  			      			padding: 15px;
+  			      		}
+  			      	}
+  			      </style>
+
+  			        </head>
+  			        <body class="markdown-body">]]
 
   			      local after = [[</body> </html>]]
 
--- src/render_html.lua.t
+++ src/render_html.lua.t
@@ -5,8 +5,28 @@ local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
 @render_markdown
 local md = require("markdown")
 encoded = md(table.concat(lines, "\n"))
+local before = [[<html>
+  <head>
+<meta http-equiv="content-type" content="text/html; charset=windows-1252">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css">
+<style>
+	.markdown-body {
+		box-sizing: border-box;
+		min-width: 200px;
+		max-width: 980px;
+		margin: 0 auto;
+		padding: 45px;
+	}
 
-local before = [[<html> <head> <meta http-equiv="content-type" content="text/html; charset=windows-1252"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css"> </head> <body class="markdown-body">]]
+	@media (max-width: 767px) {
+		.markdown-body {
+			padding: 15px;
+		}
+	}
+</style>
+
+  </head>
+  <body class="markdown-body">]]
 
 local after = [[</body> </html>]]
 
